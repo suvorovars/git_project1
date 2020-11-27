e7d5f1eebb2bbe88eef8f9f03514c8ed1c9edac0
Арсений <suvorovars@gmail.com>
2020-11-27 21:15:55 +0500

    :-)

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QPen, QColor, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('untitled.ui', self)  # Загружаем дизайн
+
+        self.pushButton.clicked.connect(self.circle)
+
+        canvas = QPixmap(600, 600)
+        self.label.setPixmap(canvas)
+
+    def circle(self):
+        x, y = [randint(10, 500) for i in range(2)]
+        d = randint(10, 100)
+        # создаем экземпляр QPainter, передавая холст (self.label.pixmap())
+        painter = QPainter(self.label.pixmap())
+        pen = QPen()
+        pen.setWidth(3)
+        pen.setColor(QColor('#fde910'))  # лимонный цвет
+        painter.setPen(pen)
+        painter.drawEllipse(x, y, d, d)
+        painter.end()
+        self.update()
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
